0e9fda662e97e60c464456775507010bf427a736
Tonin <toningega@gmail.com>
2022-08-29 12:02:52 +0100

    initialising repo

---
+++ my_battery.py
@@ -0,0 +1,25 @@
+import growattServer
+import telebot
+import yaml
+
+with open('config.yaml', 'r') as yml:
+    config = yaml.safe_load(yml)
+
+PLANT_ID = config['growatt']['PLANT_ID']
+
+# Send data to Telegram
+bot = telebot.TeleBot(config['telegram']['api_key'])
+user_id = config['telegram']['user_id']
+
+@bot.message_handler(commands=['help','hello'])
+def send_message(msg):
+    bot.reply_to(msg,'yes')
+
+@bot.message_handler(commands=['getbat'])
+def send_message(msg):
+    api = growattServer.GrowattApi()
+    login_response = api.login(config['growatt']['username'], config['growatt']['password'])
+    reply = str(api.device_list(PLANT_ID)[0]['capacity'])
+    bot.reply_to(msg,reply)
+
+bot.polling()
